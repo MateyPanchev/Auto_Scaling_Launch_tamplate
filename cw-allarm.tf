76617024fde166138ea1c9b1154f7dd39bfe242e
MateyPanchev <135972988+MateyPanchev@users.noreply.github.com>
2023-06-09 11:49:40 +0300

    Update cw-alarm.tf
    scale down alaram

--- cw-allarm.tf
+++ cw-allarm.tf
@@ -1,5 +1,5 @@
-# Create a Cloudwatch Alarm ------------------------------------------------------
-resource "aws_cloudwatch_metric_alarm" "aws-cloudwatch-metric-alarm" {
+# Create a Cloudwatch Alarm Scale Up
+resource "aws_cloudwatch_metric_alarm" "aws-cloudwatch-metric-alarm-up" {
   alarm_name          = "This will scale up"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
@@ -13,13 +13,42 @@ resource "aws_cloudwatch_metric_alarm" "aws-cloudwatch-metric-alarm" {
     aws_autoscaling_policy.aws-autoscaling-policy.arn
   ]
   dimensions = {
-    AutoScalingGroupName = var.auto-scaling-group-name
+    AutoScalingGroupName = aws_autoscaling_group.aws-autoscaling-group.name
   }
 }
 
-resource "aws_autoscaling_policy" "aws-autoscaling-policy-cloudwatch-metric-alarm" {
+# Create a Cloudwatch Alarm Policy
+resource "aws_autoscaling_policy" "aws-autoscaling-policy-cloudwatch-metric-alarm-up-policy" {
   name                   = "aws-autoscaling-policy-cloudwatch-metric-alarm"
   scaling_adjustment     = 4
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.aws-autoscaling-group.name
+}
+
+# Create a Cloudwatch Alarm Scale Down
+resource "aws_cloudwatch_metric_alarm" "aws-cloudwatch-metric-alarm-down" {
+  alarm_name          = "This will scale down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 30
+  alarm_description   = "This metric monitors EC2 CPU utilization for scale-down"
+  alarm_actions       = [
+    aws_autoscaling_policy.aws-autoscaling-policy.arn
+  ]
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.aws-autoscaling-group.name
+  }
+}
+
+# Create a Cloudwatch Alarm Policy
+resource "aws_autoscaling_policy" "aws-autoscaling-policy-cloudwatch-metric-alarm-down-policy" {
+  name                   = "scale-down-policy"
+  scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.aws-autoscaling-group.name
